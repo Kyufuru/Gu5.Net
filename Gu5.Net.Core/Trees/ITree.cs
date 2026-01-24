56fab95975e0f676084fbe2952888ccaede8a7bf
Kyufuru <kyufuru@users.noreply.github.com>
2026-01-24 11:25:38 +0800

    feat: tree

---
+++ Gu5.Net.Core/Trees/ITree.cs
@@ -0,0 +1,27 @@
+﻿namespace Gu5.Net.Core.Trees
+{
+    /// <summary>
+    /// 基础树表
+    /// </summary>
+    public interface ITree<T>
+    {
+        /// <summary>
+        /// 上级
+        /// </summary>
+        T Parent { get; set; }
+
+        /// <summary>
+        /// 下级
+        /// </summary>
+        List<T> Childs { get; set; }
+
+        /// <summary>
+        /// 下级别名
+        /// </summary>
+        List<T> Children
+        {
+            get => Childs;
+            set => Childs = value;
+        }
+    }
+}
---
+++ Gu5.Net.Core/Trees/TreeBase.cs
@@ -0,0 +1,14 @@
+﻿namespace Gu5.Net.Core.Trees
+{
+    /// <summary>
+    /// 基础树对象
+    /// </summary>
+    public class TreeBase : ITree<TreeBase>
+    {
+        /// <inheritdoc />
+        public TreeBase Parent { get; set; } = null!;
+
+        /// <inheritdoc/>
+        public List<TreeBase> Childs { get; set; } = [];
+    }
+}
