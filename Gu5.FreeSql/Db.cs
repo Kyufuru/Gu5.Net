5d7b9bbdc7813b7c4374dd7a4c7b7a0c663da8ce
佳耀 许 <497718341@qq.com>
2026-04-13 16:35:12 +0800

    feat: FreeSql Orm Helper

---
+++ Gu5.FreeSql/Db.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using FreeSql;
+using FreeSql.Internal;
+
+namespace Gu5.FreeSql
+{
+    /// <summary>
+    /// 数据库上下文
+    /// </summary>
+    public static class Db
+    {
+        private static readonly Lazy<FreeSqlCloud<string>> _lazy = new Lazy<FreeSqlCloud<string>>(NewDb);
+
+        /// <summary>
+        /// 数据库
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        public static IFreeSql Dbo<T>() => _lazy.Value.Use(typeof(T).Name);
+
+
+        /// <summary>
+        /// 配置
+        /// </summary>
+        public static Dictionary<string, string> Config { get; set; } = new Dictionary<string, string>();
+
+        /// <summary>
+        /// 初始化数据库
+        /// </summary>
+        /// <returns></returns>
+        private static FreeSqlCloud<string> NewDb()
+        {
+            Utils.IsStrict = false;
+
+            var db = new FreeSqlCloud<string>();
+
+            var tps = Assembly
+                .GetExecutingAssembly().GetTypes()
+                .Where(t =>
+                    typeof(IDb).IsAssignableFrom(t) &&
+                    !t.IsInterface && !t.IsAbstract);
+
+            foreach (var x in tps)
+            {
+                var idb = Activator.CreateInstance(x) as IDb;
+                var dbo = idb?.Register(new FreeSqlBuilder(), Config);
+                db.Register(x.Name, () => dbo);
+            }
+
+            return db;
+        }
+    }
+}
---
+++ Gu5.FreeSql/IDb.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+using FreeSql;
+
+namespace Gu5.FreeSql
+{
+    /// <summary>
+    /// 数据库通过此接口注册
+    /// </summary>
+    internal interface IDb
+    {
+        /// <summary>
+        /// 注册方法
+        /// </summary>
+        /// <param name="bd">数据库配置</param>
+        /// <param name="conns">连接字符串</param>
+        /// <returns></returns>
+        IFreeSql Register(FreeSqlBuilder bd, Dictionary<string, string> conns);
+    }
+}
